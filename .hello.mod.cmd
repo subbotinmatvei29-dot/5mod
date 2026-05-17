332c76a5bf259cf40155cdbd7acfe987a5a6c4f8
Matvei Subbotin <matvei@example.com>
2026-05-17 02:39:43 -0700

    a

--- .hello.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/matvei/kernel/hello.mod := { echo  /home/matvei/kernel/hello.o;  echo; } > /home/matvei/kernel/hello.mod
--- .modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/matvei/kernel/modules.order := {   echo /home/matvei/kernel/hello.ko; :; } | awk '!x[$$0]++' - > /home/matvei/kernel/modules.order
